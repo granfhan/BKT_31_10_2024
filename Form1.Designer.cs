0ba358f0962252da3ddd420b43451557e02c2651
granfhan <121719521+granfhan@users.noreply.github.com>
2024-10-31 15:59:00 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace BKT_2_31_10_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.them = new System.Windows.Forms.Button();
+            this.Xoa = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ListSP = new System.Windows.Forms.ListBox();
+            this.ListCart = new System.Windows.Forms.ListBox();
+            this.TongTien = new System.Windows.Forms.TextBox();
+            this.TongSL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ThanhToan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // them
+            // 
+            this.them.Location = new System.Drawing.Point(22, 386);
+            this.them.Name = "them";
+            this.them.Size = new System.Drawing.Size(182, 23);
+            this.them.TabIndex = 2;
+            this.them.Text = " Thêm vào giỏ hàng";
+            this.them.UseVisualStyleBackColor = true;
+            this.them.Click += new System.EventHandler(this.them_Click);
+            // 
+            // Xoa
+            // 
+            this.Xoa.Location = new System.Drawing.Point(229, 386);
+            this.Xoa.Name = "Xoa";
+            this.Xoa.Size = new System.Drawing.Size(182, 23);
+            this.Xoa.TabIndex = 3;
+            this.Xoa.Text = " Xóa khỏi giỏ hàng";
+            this.Xoa.UseVisualStyleBackColor = true;
+            this.Xoa.Click += new System.EventHandler(this.Xoa_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "SP1.jpg");
+            this.imageList1.Images.SetKeyName(1, "SP2.jpg");
+            this.imageList1.Images.SetKeyName(2, "SP3.jpg");
+            this.imageList1.Images.SetKeyName(3, "SP4.jpg");
+            this.imageList1.Images.SetKeyName(4, "SP5.jpg");
+            this.imageList1.Images.SetKeyName(5, "SP6.jpg");
+            // 
+            // ListSP
+            // 
+            this.ListSP.FormattingEnabled = true;
+            this.ListSP.ItemHeight = 16;
+            this.ListSP.Location = new System.Drawing.Point(22, 26);
+            this.ListSP.Name = "ListSP";
+            this.ListSP.Size = new System.Drawing.Size(287, 308);
+            this.ListSP.TabIndex = 4;
+            this.ListSP.SelectedIndexChanged += new System.EventHandler(this.ListSP_SelectedIndexChanged);
+            // 
+            // ListCart
+            // 
+            this.ListCart.FormattingEnabled = true;
+            this.ListCart.ItemHeight = 16;
+            this.ListCart.Location = new System.Drawing.Point(598, 26);
+            this.ListCart.Name = "ListCart";
+            this.ListCart.Size = new System.Drawing.Size(448, 308);
+            this.ListCart.TabIndex = 5;
+            this.ListCart.SelectedIndexChanged += new System.EventHandler(this.listCart_SelectedIndexChanged);
+            // 
+            // TongTien
+            // 
+            this.TongTien.Location = new System.Drawing.Point(691, 352);
+            this.TongTien.Name = "TongTien";
+            this.TongTien.Size = new System.Drawing.Size(100, 22);
+            this.TongTien.TabIndex = 6;
+            this.TongTien.Text = " ";
+            // 
+            // TongSL
+            // 
+            this.TongSL.Location = new System.Drawing.Point(937, 352);
+            this.TongSL.Name = "TongSL";
+            this.TongSL.Size = new System.Drawing.Size(100, 22);
+            this.TongSL.TabIndex = 7;
+            this.TongSL.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(598, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = " Tổng Tiền";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(831, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = " Tổng số lượng";
+            // 
+            // ThanhToan
+            // 
+            this.ThanhToan.Location = new System.Drawing.Point(601, 386);
+            this.ThanhToan.Name = "ThanhToan";
+            this.ThanhToan.Size = new System.Drawing.Size(123, 23);
+            this.ThanhToan.TabIndex = 10;
+            this.ThanhToan.Text = " Thanh Toán";
+            this.ThanhToan.UseVisualStyleBackColor = true;
+            this.ThanhToan.Click += new System.EventHandler(this.ThanhToan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 504);
+            this.Controls.Add(this.ThanhToan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TongSL);
+            this.Controls.Add(this.TongTien);
+            this.Controls.Add(this.ListCart);
+            this.Controls.Add(this.ListSP);
+            this.Controls.Add(this.Xoa);
+            this.Controls.Add(this.them);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button them;
+        private System.Windows.Forms.Button Xoa;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox ListSP;
+        private System.Windows.Forms.ListBox ListCart;
+        private System.Windows.Forms.TextBox TongTien;
+        private System.Windows.Forms.TextBox TongSL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ThanhToan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static BKT_2_31_10_2024.Form1;
+
+namespace BKT_2_31_10_2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProduct();
+            Display();
+        }
+
+        public class Product
+        {
+            public string TenSP { get; set;}
+            public float Gia { get; set; }
+            public int SoLuong {  get; set; }
+            public Product(string tenSP, float gia, int soLuong)
+            {
+                TenSP = tenSP;
+                Gia = gia;
+                SoLuong = soLuong;
+            }
+        }
+        public class CartItem
+        {
+            public Product SP;
+            public int SL;
+            public CartItem(Product sp, int sl)
+            {
+                SP = sp;    
+                SL = sl;
+            }
+            public float Tong => SP.Gia * SL; 
+        }
+        private void LoadProduct()
+        {
+            products.Add(new Product("Áo Vàng", 100000, 3));
+            products.Add(new Product("Áo xanh", 100000, 5));
+            products.Add(new Product("Áo Đen", 100000, 4));
+            products.Add(new Product("Áo Trắng", 100000, 5));
+            products.Add(new Product("Áo đen trắng ", 200000, 5));
+            products.Add(new Product("Ao Đỏ", 100000, 5));
+        }
+        private List<Product> products = new List<Product>();
+        private List<CartItem> cart = new List<CartItem>();
+        private void Display()
+        {
+            ListSP.Items.Clear();
+            foreach (Product p in products)
+            {
+                ListSP.Items.Add($"{p.TenSP} - {p.Gia} - Số lượng còn lại {p.SoLuong}");
+            } 
+                
+        }
+        private void DisplayCart()
+        {
+            ListCart.Items.Clear();
+            foreach (CartItem item in cart)
+            {
+                ListCart.Items.Add($"{item.SP.TenSP} - Số lượng: {item.SL} - Giá: {item.SP.Gia} - Thành tiền: {item.Tong}");
+            }
+            TongTien.Text = $"{TongSoTien():C}";
+            TongSL.Text = $"{TongSoLuong()}";
+        }
+
+        private float TongSoTien()
+        {
+            return cart.Sum(item => item.Tong);
+        }
+        private int TongSoLuong()
+        {
+            return cart.Sum(item => item.SL);
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void them_Click(object sender, EventArgs e)
+        {
+            Product selectedProduct = products[ListSP.SelectedIndex];
+            if (selectedProduct.SoLuong > 0)
+            {
+                CartItem cartItem = cart.FirstOrDefault(i => i.SP == selectedProduct);
+
+                if (cartItem != null)
+                {
+                    cartItem.SL++;
+                }
+                else
+                {
+                    cart.Add(new CartItem(selectedProduct, 1));
+                }
+
+                // Giảm số lượng sản phẩm trong kho
+                selectedProduct.SoLuong--;
+                Display();
+                DisplayCart();
+            }
+            else
+            {
+                MessageBox.Show("Sản phẩm đã hết hàng.");
+            }
+        }
+    
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DanhSachSP_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void ListSP_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listCart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void Xoa_Click(object sender, EventArgs e)
+        {
+            if (ListCart.SelectedIndex >= 0)
+            {
+                int index = ListCart.SelectedIndex;
+                CartItem selectedCartItem = cart[index];
+
+                // Tăng số lượng sản phẩm trong kho
+                selectedCartItem.SP.SoLuong += selectedCartItem.SL;
+                cart.Remove(selectedCartItem);
+                Display();
+                DisplayCart();
+            }
+        }
+
+        private void ThanhToan_Click(object sender, EventArgs e)
+        {
+            if (cart.Count > 0)
+            {
+                MessageBox.Show("Đơn hàng đã được thanh toán thành công!", "Thanh toán");
+                cart.Clear();
+                Display();
+                DisplayCart();
+            }
+            else
+            {
+                MessageBox.Show("Giỏ hàng hiện tại trống.", "Thông báo");
+            }
+        }
+    }
+}
